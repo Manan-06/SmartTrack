ea60f26c2ca8462dae62386240566cd8cc5ae55f
Manan <mananvaghela06@gmail.com>
2026-06-08 17:29:01 +0530

    add credit counter that updates on course toggle

--- scripts/main.js
+++ scripts/main.js
@@ -1,22 +1,33 @@
 console.log("JavaScript is working!");
 
+// Gets credits earned card
+const creditsEarnedEl = document.querySelector('.stat-card:first-child h2');
+let creditsEarned = 124;
+
 // Get all rows in the courses table body
 const courseRows = document.querySelectorAll('.recent-courses tbody tr');
 
 courseRows.forEach(function(row) { 
     row.addEventListener('click', function() {
-        // Get the status cell (last cell in the row)
+
+        // Get the status cell and credits for this row
         const statusCell = row.querySelector('td:last-child');
+        const rowCredits = parseInt(row.dataset.credits);
 
         //Toggle between Completed and Pending
         if (statusCell.textContent == 'Pending') {
             statusCell.textContent = 'Completed' ;
             statusCell.style.color = '#1D9E75';
             row.style.backgroundColor = '#f0faf6';
+            creditsEarned += rowCredits;
         } else if (statusCell.textContent === 'Completed') {
             statusCell.textContent = 'Pending';
             statusCell.style.color = '#BA7517';
             row.style.backgroundColor = '';
+            creditsEarned -= rowCredits;
         }
+
+        //Update the credits earned card
+        creditsEarnedEl.textContent = creditsEarned;
     });
 });
